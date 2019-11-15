184e51ac23cebd1920c39e05effa8251c2bc028c
Subha Sarkar <subha7431@gmail.com>
2019-11-15 21:18:52 +0530

    created all the static components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-            <p className="f3 link dim black underline pa3 pointernp ">Sing Out</p>
+            <p className="f3 link dim black underline pa3 pointer ">Sing Out</p>
         </nav>
     )
 }
