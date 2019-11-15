fd15db8f445787d20734829d38f68879602632cc
Subha Sarkar <subha7431@gmail.com>
2019-11-15 21:55:20 +0530

    changes some style

--- src/components/ImageLinkFrom/ImageLinkFrom.js
+++ src/components/ImageLinkFrom/ImageLinkFrom.js
@@ -10,7 +10,7 @@ const ImageLinkFrom = () => {
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
                 <input className="f4 pa2 w-70 center" type="text" />
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-blue btn-grad">Detect</button>
                 </div>
             </div>
         </div>
