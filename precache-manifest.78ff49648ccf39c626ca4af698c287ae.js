cf02aa29b4c0442032721daf247d58771d252b73
Subha Sarkar <sarkarsubha1206@gmail.com>
2019-12-02 14:02:21 +0530

    Updates

---
+++ precache-manifest.78ff49648ccf39c626ca4af698c287ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c984971c19903ee7cadf976334780c",
+    "url": "/faceoff/index.html"
+  },
+  {
+    "revision": "aa260fa60745124a2b95",
+    "url": "/faceoff/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ce387dc6635fcf45a774",
+    "url": "/faceoff/static/css/main.5bad45dc.chunk.css"
+  },
+  {
+    "revision": "aa260fa60745124a2b95",
+    "url": "/faceoff/static/js/2.7ce32b7c.chunk.js"
+  },
+  {
+    "revision": "ce387dc6635fcf45a774",
+    "url": "/faceoff/static/js/main.f0a1b530.chunk.js"
+  },
+  {
+    "revision": "0278624d85a16ce85824",
+    "url": "/faceoff/static/js/runtime-main.7d5ba714.js"
+  }
+]);
